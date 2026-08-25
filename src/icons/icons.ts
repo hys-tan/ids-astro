204b013f9e0d3e6fb272b1293fc75579525fe9ad
Eliot <ramirezeliot3-1@hotmail.com>
2026-08-25 16:54:28 -0500

    v 1.5.2 | fix: convert react to astro

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -3,7 +3,7 @@ export const ICONS = {
     mapPin:         "bi:pin-map-fill",
     clock:          "bi:clock-fill",
     arrowDown:      "bi:chevron-down",
-    arrowRight:     "bi:box-arrow-in-right",
+    arrowUpRight:   "bi:arrow-up-right-square-fill",
     personGear:     "bi:person-fill-gear",
     settings:       "bi:gear-fill",
     tool:           "bi:tools"
