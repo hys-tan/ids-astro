9e877fb3cf6190b6879f9f1c61528f8720551b76
Eliot <ramirezeliot3-1@hotmail.com>
2026-08-25 21:19:47 -0500

    v 1.6.0 | feat: implement spare parts section

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -3,6 +3,7 @@ export const ICONS = {
     mapPin:         "bi:pin-map-fill",
     clock:          "bi:clock-fill",
     arrowDown:      "bi:chevron-down",
+    arrowRight:     "bi:arrow-right-short",
     arrowUpRight:   "bi:arrow-up-right-square-fill",
     personGear:     "bi:person-fill-gear",
     settings:       "bi:gear-fill",
