ea28adfd201cf48fbc30d246439020d1ba28ab3e
Eliot <ramirezeliot3-1@hotmail.com>
2026-08-26 21:22:53 -0500

    v 1.9.0 | feat: add footer

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -7,7 +7,14 @@ export const ICONS = {
     arrowUpRight:   "bi:arrow-up-right-square-fill",
     personGear:     "bi:person-fill-gear",
     settings:       "bi:gear-fill",
-    tool:           "bi:tools"
+    tool:           "bi:tools",
+    mail:           "bi:envelope-fill",
+    phone:          "bi:telephone-fill",
+
+    //logos redes sociales
+    facebook:       "bxl:facebook",
+    instagram:      "bxl:instagram",
+    linkedIn:       "bxl:linkedin"
 } as const;
 
 export type IconName = keyof typeof ICONS;
